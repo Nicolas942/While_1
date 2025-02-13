b174b2eb9b9832ed67c9c74b8a15aa6e20cc503b
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2025-02-13 18:06:40 -0500

    Actialización

---
+++ suma_n_numeros.py
@@ -0,0 +1,15 @@
+#input
+
+n = int(input('Digite un numero: '))
+
+#Processing
+s = 0
+i = 1
+
+while i <= n:
+    s = s + i
+    i = i + 1
+
+#ouput
+
+print(f"La suma de los {n} primero números es: {s}")
